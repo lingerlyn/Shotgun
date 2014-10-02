7bc571c197504cdb7d0ee87229ab469ae6362ff9
danielso <daniel.soudry@gmail.com>
2014-10-01 21:07:28 -0400

    Added two connectivity estimation functions
    EstimateA_L1_logistic_AccurateGrad
    EstimateA_L1_logistic_sampling
    
    These functions don't work well, and are designed only for the special
    case b=0. So, don't use them
    
    Also, added the 'prob' network architecture option.

---
+++ EstimateConnectivity/EstimateA_L1_logistic_sampling.m
@@ -0,0 +1,137 @@
+function EW=EstimateA_L1_logistic_sampling(CXX,CXY,rates,sparsity,N_stim,pen_diag,warm)
+% Algorithm Implements FISTA algorithm by Beck and Teboulle 2009 for L1 linear regression
+% Gradient calculated using sampling - only works now for b=0
+
+% INPUTS: 
+% CXX - covariance
+% CXY - cross-covariance
+% rates - mean firing rates
+% sparsity - required sparsitiy level (percentage of non-zero enteries in EW)
+% N_stim - number of stimuli
+% pen_dial - 1 if we want to penalize diagonal entries; 0 otherwise
+% warm - 1 if we want to do warm starts within FISTA; 0 otherwise
+% initial - initial conditions
+% OUTPUTS:
+% EW: ML estimate of weights
+
+%params
+% iterations=300;
+
+N=length(rates);
+Tol_sparse=0.01; %tolerance for sparsity level
+Tol_FISTA=1/N^2; %tolerance for fista
+max_iterations=100;
+L_z=1e5; %number of samples
+z=bsxfun(@plus,sqrtm(CXX)*randn(N,L_z),rates);  %samples from "spikes"
+
+%initialize FISTA
+% if initial
+%     x=initial;
+% else
+    x=0*CXY';
+% end
+y=x;
+EF=x;
+
+V=-diag(pi*(rates.*log(rates)+(1-rates).*log(1-rates))/8); %for the gradient
+V(isnan(V))=0; %take care of 0*log(0) cases...
+L=2*max(V(:))*max(eig(CXX)); %lipshitz constant
+
+%initialize binary search
+lambda_high=1e1;%max(1e8,1e2*L); %maximum bound for lambda
+lambda_low=min(1e-8,1e-2*L);  %minimum bound for lambda
+loop_cond=1;  %flag for while llop
+
+while  loop_cond %binary search for lambda that give correct sparsity level
+    if sparsity==1
+        lambda=0;
+    else
+        lambda=(lambda_high+lambda_low)/2;
+    end
+
+%%% FISTA
+if ~warm
+    x=0*CXY';
+    y=x;
+end
+    
+if pen_diag
+    mask=ones(N);
+else  
+    mask=ones(N)-eye(N); % used to remove L1 penalty from diagonal
+end
+t_next=1;
+FISTA_cond=1;
+iteration=0;
+func= @(x) 1./(1+exp(-x)); %logistic function
+MXY=CXY+rates*rates'; 
+
+while FISTA_cond
+    t=t_next;
+    x_prev=x;
+    
+    for nn=1:N
+        b=0;  %need to modify this to MLE solution given the weights
+        y_nn=y;
+        y_nn(:,nn)=0;
+        U=bsxfun(@plus,y(:,nn),y_nn*z+b);
+        EF(:,nn)=rates(nn)*mean(func(U),2);
+    end
+    
+    u=y-(2/L)*(EF-MXY');
+      
+    x=ThresholdOperator(u,mask.*lambda/L);
+    if any(~isfinite(u(:)))
+        error('non finite x!')
+    end
+    t_next=(1+sqrt(1+4*t^2))/2;
+    y=x+((t-1)/t_next)*(x-x_prev);   
+    iteration=iteration+1;    
+    FISTA_cond=(mean(abs(x(:)-x_prev(:)))>Tol_FISTA);
+    if (iteration>max_iterations);
+        break
+        warning('FISTA max iteration reached');
+    end
+end
+%%% 
+
+    temp=~~x(1:(end-N_stim),1:(end-N_stim));
+    sparsity_measure=mean(temp(:));
+    cond=sparsity_measure<sparsity;
+
+    
+    loop_cond=(abs(sparsity_measure-sparsity)/sparsity >  Tol_sparse);
+    if sparsity==1
+        loop_cond=0;
+    end
+
+    if cond
+        lambda_high=lambda
+    else
+        lambda_low=lambda
+    end
+end
+        
+EW=x;
+end
+
+function y = ThresholdOperator( x , lambda )
+%THRESHOLDOPERATOR Summary of this function goes here
+% This function implements the threshold operator for the l1/l2 group norm
+% as specified on "convex optimization with sparsity-inducing norms", page 12, last equation
+
+%   Detailed explanation goes here
+% x - input
+% y - output
+% lambda - regularization constant
+
+    temp=(1-lambda./abs(x));
+    temp(isnan(temp))=1;
+    y=temp.*x;
+    y(temp<0)=0;
+    
+
+
+
+end
+
--- Misc/GetName.m
+++ Misc/GetName.m
@@ -8,7 +8,10 @@ obs=params.spike_gen.sample_ratio;
 sample_type=[];
 if strcmp(params.spike_gen.sample_type,'fixed_subset')
     sample_type='_fixed_subset';
+elseif strcmp(params.spike_gen.sample_type,'continuous')
+    sample_type='_continuous';
 end
+    
 
 
 file_name=fullfile('Results',['Run_N=' num2str(N) '_obs=' num2str(obs) '_T=' num2str(T) sample_type '.mat']);
